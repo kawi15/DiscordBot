0dc7996fd6fdce5c4b459e590dc567e66f7cf76e
KarolWiniewski1 <>
2021-05-01 09:29:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const mySecret = process.env['TOKEN']
+const client = new Discord.Client();
+const keep_alive = require('./keep_alive.js');
+const { prefix } = require('./config.json');
+
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === `ping` && !msg.author.bot) {
+    msg.channel.send("pong");
+  }
+  else if (msg.content === "pong" && !msg.author.bot) {
+    msg.channel.send("ping");
+  }
+  else if (msg.content === `${prefix}server`) {
+    msg.channel.send
+    (`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
+  }
+  else if (msg.content === `beep`) {
+    msg.channel.send(`boop`);
+  }
+})
+
+client.on("message", msg => {
+  if (msg.content === "wtf") {
+    msg.reply("co cie zdziwiło?")
+  }
+})
+
+client.login(mySecret);
