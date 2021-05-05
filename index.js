7319eb184e0528a066207db619477f3c3ed8cfbf
Karol Wiśniewski <>
2021-05-05 18:23:49 +0000

    Dodanie planowanych wiadomości - fundament pod informacje o bitwach na mapie globalnej

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const mySecret = process.env['TOKEN']
 const client = new Discord.Client();
 const keep_alive = require('./keep_alive.js');
 const { prefix } = require('./config.json');
+const cron = require('node-cron');
+const cron1 = require('cron');
+const express = require('express');
 
+/*app = express();
+
+cron.schedule('* * * * *', function() {
+  let timestamp = Date.now();
+  let date = new Date(timestamp);
+  console.log('running a task every minute');
+  console.log(date);
+  let channel = client.channels.cache.get(`693855591570997348`);
+  //channel.send('running a task every minute');
+});
+
+app.listen(3000);*/
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+
+let scheduledMessage = new cron1.CronJob('18 18 * * *', () => {
+  // This runs every day at 10:30:00, you can do anything you want
+  console.log("test")
+  let channel = client.channels.cache.get(`693855591570997348`);
+  //channel.send('test');
+});
+
+scheduledMessage.start();
 
 client.on("message", msg => {
   if (msg.content === `ping` && !msg.author.bot) {
@@ -25,9 +49,11 @@ client.on("message", msg => {
   }
 })
 
+
+
 client.on("message", msg => {
-  if (msg.content === "wtf") {
-    msg.reply("co cię zdziwiło?");
+  if (msg.content === `${prefix}coub`) {
+    msg.channel.send(`tu będą linki do coubów`)
   }
 })
 
